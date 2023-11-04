144600bb52c094fca740dadb45cab78f2c61f3c1
webber0928 <bird23074035@gmail.com>
2023-11-05 00:23:35 +0800

    set io on event

--- server/app.js
+++ server/app.js
@@ -13,3 +13,46 @@ app.use(express.static(frontendPath));
 server.listen(3000, () => {
     console.log("Server started on port 3000");
 });
+
+io.on('connection', (socket) => {
+
+    socket.on('main-join', (data) =>{
+    });
+    
+    socket.on('main-join-game', (data) => {
+    });
+    
+    socket.on('player-join', (params) => {
+    });
+    
+    socket.on('player-join-game', (data) => {
+    });
+
+    socket.on('player-answer', function(num){
+    });
+    
+    socket.on('disconnect', () => {
+    });
+    
+    socket.on('get-score', function(){
+    });
+    
+    socket.on('time', function(data){
+    });
+    
+    socket.on('time-up', function(){
+    });
+    
+    socket.on('next-question', function(){
+    });
+    
+    socket.on('start-game', () => {
+    });
+    
+    socket.on('request-db-names', function(){
+    });
+    
+    socket.on('new-quiz', function(data){
+    });
+    
+});
